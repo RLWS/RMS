01eb098ff7313f5a98e4f491c43b33c98fd460ae
RLWS靜 <1600510083@qq.com>
2019-11-27 17:35:34 +0800

    .

---
+++ src/main/java/com/rlws/rms/RmsApplication.java
@@ -0,0 +1,16 @@
+package com.rlws.rms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rlws
+ * @date 2019/11/27  13:32
+ */
+
+@SpringBootApplication
+public class RmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RmsApplication.class, args);
+    }
+}
